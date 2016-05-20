6c974dcf545e43bf50ef372cf7851b627a8fb224
Josh Marcus <josh.marcus@gmail.com>
2016-05-20 16:07:46 -0400

    Add nested case class examples.

--- src/test/scala/com/meetup/cupboard/JsonSpec.scala
+++ src/test/scala/com/meetup/cupboard/JsonSpec.scala
@@ -2,8 +2,7 @@ package com.meetup.cupboard
 
 import org.scalatest._
 import spray.json._
-
-import com.meetup.cupboard.models.Foo
+import com.meetup.cupboard.models.{ Bar, Foo }
 
 class FamilyFormatsSpec extends FlatSpec with Matchers {
   import JsonFormats._
@@ -18,6 +17,13 @@ class FamilyFormatsSpec extends FlatSpec with Matchers {
     // deserialize
     val foo1restored = fooJson.convertTo[Foo]
     foo1restored shouldBe foo1
+
+    // nested case class example
+    val bar1 = Bar(3, Foo("foo", 2))
+    val barJson = bar1.toJson
+    barJson shouldBe """{"f":{"i":2,"s":"foo"},"i":3}""".parseJson
+    val bar1restored = barJson.convertTo[Bar]
+    bar1restored shouldBe bar1
 
   }
 }
--- src/test/scala/com/meetup/cupboard/models/package.scala
+++ src/test/scala/com/meetup/cupboard/models/package.scala
@@ -2,6 +2,6 @@ package com.meetup.cupboard
 
 package object models {
   case class Foo(s: String, i: Int)
-  case class Bar(i: Int)
+  case class Bar(i: Int, f: Foo)
 }
 
