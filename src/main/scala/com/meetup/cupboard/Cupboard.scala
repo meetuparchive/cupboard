6c1ed754257a0d54cf6849d436ca339c9f4900ce
Josh Marcus <josh.marcus@gmail.com>
2016-06-03 14:10:35 -0400

    Handle errors in save and load methods.
    The signatures of save and load are changed to return Xor[Exception, C] instead
    of just C.

--- src/main/scala/com/meetup/cupboard/Cupboard.scala
+++ src/main/scala/com/meetup/cupboard/Cupboard.scala
@@ -2,11 +2,12 @@ package com.meetup.cupboard
 
 import java.time.Instant
 
+import cats.data.Xor
 import com.google.cloud.datastore.{Datastore, Entity, ReadOption}
 import com.meetup.cupboard.DatastoreFormats.{DatastoreFormat, InstantDatastoreProperty}
 
 object Cupboard {
-  def save[C](ds: Datastore, caseClass: C, kind: String)(implicit cf: DatastoreFormat[C]): Persisted[C] = {
+  def save[C](ds: Datastore, caseClass: C, kind: String)(implicit cf: DatastoreFormat[C]): Xor[Exception, Persisted[C]] = {
     val keyFactory = ds.newKeyFactory().kind(kind)
     val key = ds.allocateId(keyFactory.newKey())
     val eBuilder = Entity.builder(key)
@@ -19,24 +20,29 @@ object Cupboard {
 
     ds.put(e.build())
 
-    Persisted(Long.unbox(key.id()), caseClass, now, now)
+    //TODO: catch failure above
+    Xor.Right(Persisted(Long.unbox(key.id()), caseClass, now, now))
   }
 
-  def save[C](ds: Datastore, caseClass: C)(implicit cf: DatastoreFormat[C]): Persisted[C] = {
+  def save[C](ds: Datastore, caseClass: C)(implicit cf: DatastoreFormat[C]): Xor[Exception, Persisted[C]] = {
     val kind = caseClass.getClass().getSimpleName
     save(ds, caseClass, kind)
   }
 
-  def load[C](ds: Datastore, id: Long, kind: String)(implicit cf: DatastoreFormat[C]): Persisted[C] = {
+  //TODO: do we want a custom error type?
+  def load[C](ds: Datastore, id: Long, kind: String)(implicit cf: DatastoreFormat[C]): Xor[Exception, Persisted[C]] = {
     val key = ds.newKeyFactory()
       .kind(kind)
       .newKey(id)
 
-    val entity: Entity = ds.get(key, Array.empty[ReadOption]: _*)
-    val caseClass: C = cf.fromEntity(entity)
+    val entityOpt: Option[Entity] = Option(ds.get(key, Array.empty[ReadOption]: _*))
 
-    val modified = InstantDatastoreProperty.getValueFromEntity("modified", entity)
-    val created = InstantDatastoreProperty.getValueFromEntity("created", entity)
-    Persisted(id, caseClass, modified, created)
+    entityOpt.map { entity =>
+      val caseClass: C = cf.fromEntity(entity)
+
+      val modified = InstantDatastoreProperty.getValueFromEntity("modified", entity)
+      val created = InstantDatastoreProperty.getValueFromEntity("created", entity)
+      Xor.Right(Persisted(id, caseClass, modified, created))
+    }.getOrElse(Xor.Left(new RuntimeException(s"Did not find entity with id $id")))
   }
 }
--- src/test/scala/com/meetup/cupboard/DatastoreSpec.scala
+++ src/test/scala/com/meetup/cupboard/DatastoreSpec.scala
@@ -6,6 +6,7 @@ import com.google.cloud.datastore.{Datastore, Entity, DateTime => GDateTime}
 import org.scalatest._
 // spray.json._
 import com.meetup.cupboard.models.{Bar, Subscription, _}
+import cats.data.Xor
 
 class DatastoreSpec extends FlatSpec with Matchers with AdHocDatastore {
 
@@ -15,13 +16,18 @@ class DatastoreSpec extends FlatSpec with Matchers with AdHocDatastore {
     withDatastore() { ds =>
       val z = Foo("hi", 3)
 
-      val z1: Persisted[Foo] = Cupboard.save(ds, z, "customKind")
-      val z2: Persisted[Foo] = Cupboard.save(ds, z)
+      val z1X = Cupboard.save(ds, z, "customKind")
+      val z2X = Cupboard.save(ds, z)
 
-      val z1restored: Persisted[Foo] = Cupboard.load[Foo](ds, z1.id, "customKind")
-      z1 shouldBe z1restored
-      val z2restored: Persisted[Foo] = Cupboard.load[Foo](ds, z2.id, "Foo")
-      z2 shouldBe z2restored
+      val z1id = z1X.getOrElse(fail()).id
+      val z2id = z2X.getOrElse(fail()).id
+
+      val z1restored = Cupboard.load[Foo](ds, z1id, "customKind")
+      z1X shouldBe z1restored
+
+      val z2 = z2X.getOrElse(fail())
+      val z2restored = Cupboard.load[Foo](ds, z2id, "Foo")
+      z2X shouldBe z2restored
     }
   }
 }
