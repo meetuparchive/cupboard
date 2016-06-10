9184b9050cf04fb08f1385c8e61a5911874e52ed
Josh Marcus <josh.marcus@gmail.com>
2016-06-10 09:47:27 -0400

    Remove commented out code.

--- src/main/scala/com/meetup/cupboard/datastore/DatastoreProperties.scala
+++ src/main/scala/com/meetup/cupboard/datastore/DatastoreProperties.scala
@@ -10,11 +10,11 @@ import com.meetup.cupboard.Result
 import scala.collection.JavaConversions._
 
 /**
-  *   DatastoreProperties define how to translate individual values from the case class type to the datastore type.
-  *
-  *   We extend LowPriorityProperties as a way of setting the priority order of typeclass instances where more than
-  *   one can apply.
-  */
+ *   DatastoreProperties define how to translate individual values from the case class type to the datastore type.
+ *
+ *   We extend LowPriorityProperties as a way of setting the priority order of typeclass instances where more than
+ *   one can apply.
+ */
 trait DatastoreProperties extends LowPriorityProperties {
   implicit def SeqEntityProperty[E](implicit entityDatastoreFormat: DatastoreFormat[E]): DatastoreProperty[List[E], java.util.List[FullEntity[_]]] = {
     new DatastoreProperty[List[E], java.util.List[FullEntity[_]]] {
@@ -88,6 +88,7 @@ trait LowPriorityProperties {
         ZonedDateTime.from(Instant.ofEpochMilli(millis).atOffset(ZoneOffset.UTC))
       }
     }
+
     def setEntityProperty(v: ZonedDateTime, name: String, e: Entity.Builder) = {
       e.set(name, GDateTime.copyFrom(java.util.Date.from(v.toInstant())))
     }
@@ -100,6 +101,7 @@ trait LowPriorityProperties {
         Instant.ofEpochMilli(millis)
       }
     }
+
     def setEntityProperty(v: Instant, name: String, e: Entity.Builder) = {
       e.set(name, GDateTime.copyFrom(java.util.Date.from(v)))
     }
@@ -111,47 +113,11 @@ trait LowPriorityProperties {
         val internalEntity: FullEntity[_] = e.getEntity(name)
         entityDatastoreFormat.fromEntity(internalEntity)
       }
+
       def setEntityProperty(v: E, name: String, e: Entity.Builder): Entity.Builder = {
         val emptyEntity = FullEntity.builder()
         val newEntity = entityDatastoreFormat.buildEntity(v, e)
         e.set(name, newEntity.build())
       }
     }
-
-  /*  implicit def SeqEntityProperty[E](implicit entityDatastoreFormat: DatastoreFormat[E]): DatastoreProperty[List[E], java.util.List[FullEntity[_]]] = {
-    new DatastoreProperty[List[E], java.util.List[FullEntity[_]]] {
-      def getValueFromEntity(name: String, e: FullEntity[_]) = {
-        val internalEntities: java.util.List[EntityValue] = e.getList[EntityValue](name)
-        val entities = internalEntities.map(e => entityDatastoreFormat.fromEntity(e.get())).toList
-        sequence(entities)
-      }
-
-      def setEntityProperty(v: List[E], name: String, e: Entity.Builder): Entity.Builder = {
-        val entities = v.map { x =>
-          val emptyEntity = FullEntity.builder()
-          val newEntity = entityDatastoreFormat.buildEntity(x, e).build()
-          new EntityValue(newEntity)
-        }
-        //com.google.cloud.datastore.Value.
-        import scala.collection.JavaConversions._
-
-        val entities2 = entities.toBuffer
-        e.set(name, entities2)
-      }
-      import PartialFunction._
-
-      // turn a list of Xors into an Xor of a list
-      def sequence(input: List[Xor[Throwable, E]]): Xor[Throwable, List[E]] = {
-        input.foldRight[Xor[Throwable, List[E]]](Xor.Right(Nil)) { (o, ol) =>
-          (o, ol) match {
-            case (Xor.Right(x), Xor.Right(xs)) => Xor.Right(x :: xs)
-            case (Xor.Left(y), _) => Xor.Left(y)
-            case (_, Xor.Left(z)) => Xor.Left(z)
-          }
-        }
-      }
-    }
-
-  } */
-
 }
