c8d12a21983f8e8056f51295a68ababd0ccb7d04
Josh Marcus <josh.marcus@gmail.com>
2016-07-15 16:25:31 -0400

    Add comment.

--- src/test/scala/com/meetup/cupboard/tests/MacroSpec.scala
+++ src/test/scala/com/meetup/cupboard/tests/MacroSpec.scala
@@ -13,12 +13,20 @@ import com.meetup.cupboard.datastore.DatastoreProperty
 
 class MacroSpec extends FlatSpec with Matchers {
   "Persistable trait" should "extract class field info & conversion instances" in {
+    /**
+     * Given:
+     *
+     * case class User(memberId: Int, username: String, createdAt: java.time.Instant)
+     * object Users extends Persistable[User]
+     */
 
+    // The macro has extracted and created a Properties object which includes:
     val expectedProperties = List(
       Property[Int]("memberId"),
       Property[String]("username"),
       Property[Instant]("createdAt")
     )
+
     User.properties.all shouldBe expectedProperties
 
     User.properties.createdAt shouldBe Property[Instant]("createdAt")
