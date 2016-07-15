cb532c298c467f3d240def315295118482d3844c
Josh Marcus <josh.marcus@gmail.com>
2016-07-15 15:54:53 -0400

    Add test for propertyConverter.

--- src/test/scala/com/meetup/cupboard/tests/MacroSpec.scala
+++ src/test/scala/com/meetup/cupboard/tests/MacroSpec.scala
@@ -3,8 +3,13 @@ package com.meetup.cupboard.tests
 import org.scalatest._
 import com.meetup.cupboard.models._
 import com.meetup.cupboard.datastore.DatastoreProperties._
+import com.meetup.cupboard.datastore.DatastoreProperties.InstantDatastoreProperty
+import com.google.cloud.datastore.{DateTime => GDateTime}
 import com.meetup.cupboard.{Persistable, Property}
 import java.time.Instant
+
+import com.google.cloud.datastore.DateTime
+import com.meetup.cupboard.datastore.DatastoreProperty
 
 class MacroSpec extends FlatSpec with Matchers {
   "Persistable trait" should "extract class field info & conversion instances" in {
@@ -19,5 +24,7 @@ class MacroSpec extends FlatSpec with Matchers {
     User.properties.createdAt shouldBe Property[Instant]("createdAt")
 
     User.properties.createdAt.name shouldBe "createdAt"
+
+    User.properties.createdAt.getPropertyConverter shouldBe a[DatastoreProperty[_, _]]
   }
 }
