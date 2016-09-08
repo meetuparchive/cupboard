13cba2d052c9b11da2a1a12584ff512d77c69184
Josh Marcus <josh.marcus@gmail.com>
2016-09-08 16:51:08 -0400

    Debug messages for travis build.

--- src/test/scala/com/meetup/cupboard/AdHocDatastore.scala
+++ src/test/scala/com/meetup/cupboard/AdHocDatastore.scala
@@ -8,20 +8,29 @@ import scala.util.control.NonFatal
 
 trait AdHocDatastore extends Suite {
   def withDatastore[A](consistency: Double = .5)(f: Datastore => A): A = {
+    println("Preparing to start up local datastore.")
     var lds: LocalDatastoreHelper = null
     try {
       lds = LocalDatastoreHelper.create(consistency)
+      println("Local datastore created.")
       lds.start()
+      println("Local datastore started.")
       val a = f(lds.options().service())
+      println("Test function completed.")
       lds.stop()
+      println("Datastore stopped.")
       a
     } catch {
-      case e: TestFailedException => fail(e)
+      case e: TestFailedException => {
+        println("Test failed exception.")
+        fail(e)
+      }
       case NonFatal(e) => {
         e.printStackTrace()
         cancel(e)
       }
     } finally {
+      println("In finally block of AdHocDatastore.")
       if (lds != null)
         lds.stop()
     }
