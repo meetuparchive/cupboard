93b4a4c4ee326b00a87c2080c679cdf8fc15257b
Josh Marcus <josh.marcus@gmail.com>
2016-09-12 11:08:03 -0400

    Don't start datastore if cached datastore exists.

--- src/test/scala/com/meetup/cupboard/AdHocDatastore.scala
+++ src/test/scala/com/meetup/cupboard/AdHocDatastore.scala
@@ -1,23 +1,42 @@
 package com.meetup.cupboard
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import com.google.cloud.datastore.Datastore
 import com.google.cloud.datastore.testing.LocalDatastoreHelper
 import org.scalatest.Suite
 import org.scalatest.exceptions.TestFailedException
+
 import scala.util.control.NonFatal
 
+object AdHocDatastore {
+  var cachedDatastore: Option[Datastore] = None
+  var count: Int = 0
+}
 trait AdHocDatastore extends Suite {
   def withDatastore[A](consistency: Double = .5)(f: Datastore => A): A = {
     println("Preparing to start up local datastore.")
     var lds: LocalDatastoreHelper = null
     try {
-      lds = LocalDatastoreHelper.create(consistency)
-      println("Local datastore created.")
-      lds.start()
-      println("Local datastore started.")
-      val a = f(lds.options().service())
+      val ds: Datastore = AdHocDatastore.cachedDatastore match {
+        case None => {
+          lds = LocalDatastoreHelper.create(consistency)
+          println("Local datastore created.")
+          lds.start()
+          println("Local datastore started.")
+          val cached = lds.options().service()
+          AdHocDatastore.cachedDatastore = Some(cached)
+          AdHocDatastore.count += 1
+          cached
+        }
+        case Some(cached) => {
+          AdHocDatastore.count += 1
+          cached
+        }
+      }
+      val a = f(ds)
       println("Test function completed.")
-      lds.stop()
+      //lds.stop()
       println("Datastore stopped.")
       a
     } catch {
@@ -30,9 +49,9 @@ trait AdHocDatastore extends Suite {
         cancel(e)
       }
     } finally {
+      AdHocDatastore.count -= 1
       println("In finally block of AdHocDatastore.")
-      if (lds != null)
-        lds.stop()
+      if (count == 0) lds.stop()
     }
   }
 }
